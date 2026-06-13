9cc9c92dc4f56f9008319f334d23e6c2d88018ba
Oleksandr Pryimak <pryimaksasha@gmail.com>
2026-06-13 17:18:23 +0200

    CharacterBase

---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright DSHNL
+
+#pragma once
+
+#include "CoreMinimal.h"
+
---
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright DSHNL
+
+
+#include "AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
